ad2a3dfcbe902c041e2bba41b72dd8eca89ad82b
Fedor <farlep1970@yandex.ru>
2022-08-12 23:37:04 +0300

    task_10

---
+++ task_10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите целое трехзначное число: ");
+string? numberString = Console.ReadLine();
+int lenght = numberString!.Length;
+while (lenght != 3)
+{
+    Console.Write("Число введено не верно, повторите ввод: ");
+    numberString = Console.ReadLine();
+    lenght = numberString!.Length;
+}
+int number = int.Parse(numberString!);
+int secondDiqit = number / 10 % 10;
+Console.WriteLine("Вторая цифра числа: " + secondDiqit);
