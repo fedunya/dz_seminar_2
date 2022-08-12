91f35d230b5b81892414b4cbd5bf220864cc9d94
Fedor <farlep1970@yandex.ru>
2022-08-13 00:35:28 +0300

    task_15

---
+++ task_15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите порядковый номер дня недели: ");
+string? numberString = Console.ReadLine();
+int number = int.Parse(numberString!);
+while (number > 7 || number < 1)
+{
+    Console.Write("Число введено не верно, повторите ввод: ");
+    numberString = Console.ReadLine();
+    number = int.Parse(numberString!);
+}
+Console.WriteLine("Это выходной день?");
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("Да");
+} 
+else
+{
+    Console.WriteLine("Нет");
+}
